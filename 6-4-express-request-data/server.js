c34f706b82b6f2f424e59bfc7403ac2667270c29
batool3-A <s202160170@kfupm.edu.sa>
2026-04-08 11:26:58 +0300

    TODO 1-2-3

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -108,14 +108,36 @@ LAB SETUP INSTRUCTIONS
 
 
 // create express app instance to create web server
-
+const express = require('express');
+const app = express();
+app.listen(3000, ()=> console.log("API running at http://localhost:3000"));
 
 
 // Query params: /echo?name=Ali&age=22
+app.get("/echo", (req, res) => {
+  const { name, age } = req.query;
 
+  if (!name || !age) {
+    return res.status(400).json({
+      ok: false,
+      error: "name & age required"
+    });
+  }
+
+  return res.json({
+    ok: true, name, age, msg: `Hello ${name}, you are ${age}`
+  });
+});
 
 // Route params: /profile/First/Last
+app.get("/profile/:first/:last", (req, res) => {
+  const { first, last } = req.params;
 
+  return res.json({
+    ok: true,
+    fullName: `${first} ${last}`
+  });
+});
 
 // Route param middleware example: /users/42
 
